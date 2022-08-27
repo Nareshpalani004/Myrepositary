e02d101c672d34eba8e287c110cbadcac719ce0f
Nareshpalani004 <112233997+Nareshpalani004@users.noreply.github.com>
2022-08-27 11:27:09 +0530

    Add files via upload

---
+++ MyArray.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class MyArray {
+	
+	String  [] myFriends = {"Gnanavel","Franklin","Srinath"};
+	
+	int x = 90;
+
+	public static void main(String[] args) {
+		
+		int[] myMarks = {90,80,75,88,77,84,89};
+		
+		System.out.println("myMarks="+myMarks[0]);
+		System.out.println("myMarks="+myMarks[3]);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
